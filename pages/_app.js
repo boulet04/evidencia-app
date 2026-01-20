5f2a75af6506f349ad9712685b04feacc7cf398e
boulet04 <massabuau95@gmail.com>
2026-01-20 09:45:52 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,26 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Charge le design system partagé */}
         <link rel="stylesheet" href="/brand.css" />
-        <meta name="theme-color" content="#050608" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      <style jsx global>{`
+        html,
+        body {
+          margin: 0 !important;
+          padding: 0 !important;
+          background: #05060a !important;
+          overflow-x: hidden;
+        }
+
+        /* évite les micro-bords dus à certains éléments */
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+
       <Component {...pageProps} />
     </>
   );
